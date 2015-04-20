a4eb81552aa2970fb10e3b88661063fe418d7bac
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-20 15:52:08 -0400

    while loop, increasing values

---
+++ tutorial4/src/tutorial4/Application.java
@@ -0,0 +1,12 @@
+package tutorial4;
+
+public class Application {
+	public static void main(String[] args) {
+		int value = 0;
+		
+		while(value < 10) {
+			System.out.println("Hello " + value);
+			value += 1;
+		}
+	}
+}
