40a0f923c218a4f0f0f3b2e3bffee99ee3984cba
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-21 09:33:41 -0400

    added else if and else conditions

---
+++ tutorial6/src/tutorial6/Application.java
@@ -0,0 +1,18 @@
+package tutorial6;
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 15;
+		
+		if(myInt < 10) {
+	      System.out.println("Yes, it's true");
+		}
+		else if(myInt > 20) {
+		  System.out.println("No, it's not true");
+		}
+		else {
+			System.out.println("None of the above");
+		}
+		  
+	}
+}
